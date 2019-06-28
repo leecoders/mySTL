6eb9ffc859262010c75de4138a6a6a98a0031f9f
leecoders <als-1994@hanmail.net>
2019-06-28 19:38:10 +0900

    qsort(), merge_sort() in sort.cpp 추가

---
+++ sort.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+const int MAX_SIZE = 1000000;
+
+int ms[MAX_SIZE+1], qs[MAX_SIZE+1], buf[MAX_SIZE+1];
+
+// qsort와 다르게 배열의 길이를 받음
+// 왜? 전체 배열이 아닌 항상 같은 크기로 분할된 부분 배열을 넘겨받기 때문
+void merge_sort(int *p, int len) {
+    if(len < 2) return; // 1개로 쪼개질 때까지 분할! -> 합칠 일만 남음
+    int i, j, k, mid;
+    mid = len/2;
+    i=0, j=mid, k=0; // k는 버퍼를 위한 인덱스
+
+    merge_sort(p, mid); // qsort와 달리 쪼개는 것을 먼저함
+    merge_sort((p + mid), (len - mid)); // (p+mid) is &p[mid]
+
+    while(i < mid && j < len) { // 끝나면 보통 둘 중 하나는 끝까지 못 갔음
+        if(p[i] < p[j]) {
+            buf[k++] = p[i++];
+        }
+        else {
+            buf[k++] = p[j++];
+        }
+    }
+    while(i < mid) { // 뭔지 모르겠지만 남은 것 버퍼에 넣어줌
+        buf[k++] = p[i++];
+    }
+    while(j < len) { // 얘도
+        buf[k++] = p[j++];
+    }
+    for(int i=0; i<len; ++i) {
+        p[i] = buf[i]; // p는 원 배열의 부분 배열이기 때문에 p[0] is
+    }
+}
+
+void qsort(int *p, int left, int right) {
+    if(left >= right) return; // left==right는 길이 1, left>right는 길이 2 이상. 종료
+    int l = left - 1;
+    int r = right + 1;
+    int mid = p[(l+r) / 2]; // 가운데 피벗
+    while(1) {
+        while(p[++l] < mid); // mid보다 왼쪽인데 크거나 같은 요소 찾으면 stop
+        while(p[--r] > mid); // mid보다 오른쪽인데 작거나 같은 요소 찾으면 stop
+        if(l >= r) break;
+        int temp = p[l];
+        p[l] = p[r];
+        p[r] = temp;
+    }
+    qsort(p, left, l-1);
+    qsort(p, r+1, right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; ++i) {
+        scanf("%d", &qs[i]);
+    }
+    merge_sort(qs, n);
+    for(int i=0; i<n; ++i) {
+        printf("%d\n", qs[i]);
+    }
+}
