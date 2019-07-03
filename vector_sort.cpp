987376233ae6b00224b4b6904542ae2b046873dc
leecoders <als-1994@hanmail.net>
2019-07-03 22:39:31 +0900

    vector_sort.cpp 추가

--- vector_sort.cpp
+++ vector_sort.cpp
@@ -63,15 +63,64 @@ struct MY_VECTOR {
 };
 
 int n;
-MY_VECTOR my_vector;
+MY_VECTOR my_vector(1000000), buf(1000000);
+
+void qsort(int *p, int left, int right) {
+    if(left >= right) return;
+    int l = left - 1;
+    int r = right + 1;
+    int mid = p[(l+r)/2];
+    
+    while(1) {
+        while(p[++l] < mid);
+        while(p[--r] > mid);
+        if(l >= r) break;
+        int temp = p[l];
+        p[l] = p[r];
+        p[r] = temp;
+    }
+    
+    qsort(p, left, l-1);
+    qsort(p, r+1, right);
+}
+
+void merge_sort(int *p, int len) {
+    if(len < 2) return;
+    int i, j, k, mid;
+    mid = len / 2;
+    i = 0, j = mid, k = 0;
+    
+    merge_sort(p, mid);
+    merge_sort(p + mid, len - mid);
+    
+    while(i < mid && j < len) {
+        if(p[i] < p[j]) {
+            buf[k++] = p[i++];
+        }
+        else {
+            buf[k++] = p[j++];
+        }
+    }
+    while(i < mid) {
+        buf[k++] = p[i++];
+    }
+    while(j < len) {
+        buf[k++] = p[j++];
+    }
+    for(int i=0; i<len; ++i) {
+        p[i] = buf[i];
+    }
+}
+
 
 int main() {
     scanf("%d", &n);
+    
     for(int i=0; i<n; ++i) {
-        int input;
-        scanf("%d", &input);
-        my_vector.push_back(input);
+        scanf("%d", &my_vector[i]);
     }
+    qsort(&my_vector[0], 0, n-1);
+    
     for(int i=0; i<n; ++i) {
         printf("%d ", my_vector[i]);
     }
